78944279991c36761b7324ae79cbc4ee182b5190
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-05-31 22:11:52 +0530

    Project submit

---
+++ Nanasa_Management_System/College Management System/Load.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace College_Management_System
+{
+    partial class Load
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Load));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.MyProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // MyProgressBar
+            // 
+            this.MyProgressBar.Location = new System.Drawing.Point(7, 148);
+            this.MyProgressBar.Name = "MyProgressBar";
+            this.MyProgressBar.Size = new System.Drawing.Size(436, 10);
+            this.MyProgressBar.TabIndex = 5;
+            this.MyProgressBar.Click += new System.EventHandler(this.MyProgressBar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(171, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nanasa Institute";
+            // 
+            // Load
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(450, 163);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MyProgressBar);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Load";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Load";
+            this.Load += new System.EventHandler(this.Load_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar MyProgressBar;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Nanasa_Management_System/College Management System/Load.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace College_Management_System
+{
+    public partial class Load : Form
+    {
+        public Load()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Load_Load(object sender, EventArgs e)
+        {
+            // ✅ Test database connection when form loads
+            try
+            {
+                string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    MessageBox.Show("✅ Connected to database: " + conn.Database, "Database Connection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Database connection failed:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Load_Load_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        int startpos = 0;
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            startpos += 1;
+            MyProgressBar.Value = startpos;
+            if (MyProgressBar.Value == 60)
+            {
+                MyProgressBar.Value = 0;
+                timer1.Stop();
+                Login log = new Login();
+                log.Show();
+                this.Hide();
+            }
+        }
+
+        private void MyProgressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
